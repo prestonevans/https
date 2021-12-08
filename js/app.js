d766d5a47d035712620d63f5fc72526711d5595a
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-12-08 08:24:37 -0700

    my first https request

--- js/app.js
+++ js/app.js
@@ -1,2 +1,37 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const https = require('https')
+const fs = require('fs')
+const { METHODS} = require('http')
+
+const options = {
+    hostname: 'en.wikipedia.org',
+    port: 443,
+    path: '/wiki/George_Washington',
+    method: 'GET'
+}
+
+const request = https.request(options, response => {
+    let responseBody = '';
+    
+
+    console.log('Response from server started')
+    console.log(`Server status: ${response.statusCode}`)
+    console.log(`Server status: ${response.headers}`)
+
+    response.setEncoding('utf-8')
+
+    response.once('data', chunk => {
+        console.log(chunk);
+    })
+    response.on('data', chunk => {
+        responseBody += chunk;
+        console.log(`chunk --${chunk.length}`)
+    })
+    response.on('end', () => {
+        fs.writeFile('george-washington.html',responseBody, err => {
+            if (err) throw err;
+            console.log('file downloaded')
+        })
+    })
+})
+
+request.end();
